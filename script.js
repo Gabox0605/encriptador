97174a811c627cfeb30e8bb15cdd9ef93c9269e8
Gabriel Mena Muñoz <144412742+Gabox0605@users.noreply.github.com>
2023-09-08 17:31:11 -0600

    Add files via upload
    archivo index.html y script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function encriptar(){
+
+    var frase = document.getElementById("textoEncriptado").Value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img,"enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img,"ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img,"imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img,"ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img,"ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML= textoEncriptado
+}
